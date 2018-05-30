Toptranslation/toptranslation_ruby

commit a058bc3521b69ba79b1489caefff40d2595ac193

style: follow convention that module hierarchy follows folder structure

lib/toptranslation/client.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation
   class Client
     def initialize(options)
       @connection = ToptranslationApi::Connection.new(options)

lib/toptranslation/resource/document.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class Document
     attr_reader :identifier, :string_count, :has_missing_strings, :translations, :updated_at, :created_at
     attr_accessor :name, :path

lib/toptranslation/resource/document_list.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class DocumentList
     include Enumerable
 

lib/toptranslation/resource/project.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class Project
     attr_reader :identifier, :created_at, :locales, :source_locale
     attr_accessor :name

lib/toptranslation/resource/project_document_list.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class ProjectDocumentList < DocumentList
     def create(name, path, options = {})
       response = @connection.post("/projects/#{@options[:project_identifier]}/documents", options.merge(name: name, path: path))

lib/toptranslation/resource/project_list.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class ProjectList
     include Enumerable
 

lib/toptranslation/resource/string_list.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class StringList
     include Enumerable
 

lib/toptranslation/resource/translation_list.rb
@@ -1,4 +1,4 @@
-module ToptranslationApi
+module Toptranslation::Resource
   class TranslationList
     include Enumerable
 

commit 0d1b13034fb6f24972a481eeed35a8d7bf7b6d05

fix: fix all resources that have not been migrated to v2 yet
This was broken in a058bc3 while restructuring the project.

lib/toptranslation/client.rb
@@ -1,23 +1,23 @@
 module Toptranslation
   class Client
     def initialize(options)
-      @connection = ToptranslationApi::Connection.new(options)
+      @connection = Connection.new(options)
     end
 
     def orders(options = {})
-      @order_list ||= ToptranslationApi::OrderList.new(@connection, options)
+      @order_list ||= Resource::OrderList.new(@connection, options)
     end
 
     def translations(options = {})
-      @translation_list ||= ToptranslationApi::TranslationList.new(@connection, options)
+      @translation_list ||= Resource::TranslationList.new(@connection, options)
     end
 
     def projects(options = {})
-      @project_list ||= ToptranslationApi::ProjectList.new(@connection, options)
+      @project_list ||= Resource::ProjectList.new(@connection, options)
     end
 
     def locales(options = {})
-      @locale_list ||= ToptranslationApi::LocaleList.new(@connection, options)
+      @locale_list ||= Resource::LocaleList.new(@connection, options)
     end
   end
 end

lib/toptranslation/resource/document.rb
@@ -32,7 +32,7 @@ module Toptranslation::Resource
     end
 
     def strings
-      ToptranslationApi::StringList.new(@connection, document_identifier: @identifier)
+      StringList.new(@connection, document_identifier: @identifier)
     end
 
     private

lib/toptranslation/resource/document_list.rb
@@ -9,7 +9,7 @@ module Toptranslation::Resource
 
     def find(identifier)
       result = @connection.get("/documents/#{identifier}")
-      ToptranslationApi::Document.new(@connection, result)
+      Document.new(@connection, result)
     end
 
     def each

lib/toptranslation/resource/project.rb
@@ -28,11 +28,11 @@ module Toptranslation::Resource
     end
 
     def documents
-      ToptranslationApi::ProjectDocumentList.new(@connection, project_identifier: @identifier)
+      ProjectDocumentList.new(@connection, project_identifier: @identifier)
     end
 
     def strings
-      ToptranslationApi::StringList.new(@connection, project_identifier: @identifier)
+      StringList.new(@connection, project_identifier: @identifier)
     end
 
     def save

lib/toptranslation/resource/project_document_list.rb
@@ -2,13 +2,13 @@ module Toptranslation::Resource
   class ProjectDocumentList < DocumentList
     def create(name, path, options = {})
       response = @connection.post("/projects/#{@options[:project_identifier]}/documents", options.merge(name: name, path: path))
-      ToptranslationApi::Document.new(@connection, response)
+      Document.new(@connection, response)
     end
 
     def create_batch(documents)
       response = @connection.post("/projects/#{@options[:project_identifier]}/documents/batch", documents: documents)
 
-      response.map { |document_attr| ToptranslationApi::Document.new(@connection, document_attr) }
+      response.map { |document_attr| Document.new(@connection, document_attr) }
     end
 
     private

lib/toptranslation/resource/project_list.rb
@@ -9,11 +9,11 @@ module Toptranslation::Resource
 
     def find(identifier)
       result = @connection.get("/projects/#{identifier}")
-      ToptranslationApi::Project.new(@connection, result)
+      Project.new(@connection, result)
     end
 
     def create(options = {})
-      ToptranslationApi::Project.new(@connection, options)
+      Project.new(@connection, options)
     end
 
     def each

lib/toptranslation/resource/string_list.rb
@@ -9,7 +9,7 @@ module Toptranslation::Resource
 
     def find(identifier)
       result = @connection.get("/strings/#{identifier}")
-      ToptranslationApi::Document.new(@connection, result)
+      Document.new(@connection, result)
     end
 
     def each
@@ -20,7 +20,7 @@ module Toptranslation::Resource
       project_identifier = options['project_identifier'] || @options[:project_identifier]
       document_identifier = options['document_identifier'] || @options[:document_identifier]
 
-      ToptranslationApi::String.new(@connection, options.merge('project_identifier' => project_identifier, 'document_identifier' => document_identifier))
+      String.new(@connection, options.merge('project_identifier' => project_identifier, 'document_identifier' => document_identifier))
     end
 
     private

lib/toptranslation/resource/translation_list.rb
@@ -9,11 +9,11 @@ module Toptranslation::Resource
 
     def find(identifier)
       result = @connection.get("/translations/#{identifier}")
-      ToptranslationApi::Translation.new(@connection, result)
+      Translation.new(@connection, result)
     end
 
     def each
-      translations.each { |translation| yield ToptranslationApi::Translation.new(@connection, translation) }
+      translations.each { |translation| yield Translation.new(@connection, translation) }
     end
 
     private

lib/toptranslation/version.rb
@@ -1,3 +1,3 @@
-module ToptranslationApi
+module Toptranslation
   VERSION = '0.7.23'.freeze
 end

toptranslation_api.gemspec
@@ -5,7 +5,7 @@ require 'toptranslation/version'
 
 Gem::Specification.new do |s|
   s.name        = 'toptranslation_api'
-  s.version     = ToptranslationApi::VERSION
+  s.version     = Toptranslation::VERSION
   s.summary     = 'A ruby client for the Toptranslation API'
   s.description = 'Allows to create and control translation projects on Toptranslation via the Toptranslation API.'
   s.authors     = ['Toptranslation GmbH']